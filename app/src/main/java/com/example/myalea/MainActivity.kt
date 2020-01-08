2c9c891385dbc6a1428b06007c108f9a54b4afbd
aldryarief <aldreva@gmail.com>
2020-01-08 21:15:52 +0700

    try catch

--- app/src/main/java/com/example/myalea/MainActivity.kt
+++ app/src/main/java/com/example/myalea/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.myalea
 
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,26 +33,27 @@ class MainActivity : AppCompatActivity() {
 
 
         var buttonProses = findViewById(R.id.buttonProses) as Button
+        var Gaji : Int; var Hari : Int; var Jam : Int; var Laine : Int
+
 
         buttonProses.setOnClickListener{
-            var Gaji : Int;var Hari : Int;var Jam : Int;var Laine : Int
-            Gaji = TotalGaji.getText().toString().toInt()
-            Hari = TotalHari.getText().toString().toInt()
-            Jam = TotalJam.getText().toString().toInt()
-            Laine = Lain.getText().toString().toInt()
+            try {
+                Gaji = TotalGaji.getText().toString().toInt()
+                Hari = TotalHari.getText().toString().toInt()
+                Jam = TotalJam.getText().toString().toInt()
+                Laine = Lain.getText().toString().toInt()
 
-            if (Gaji == 0) {
-                Toast.makeText(this, "Gaji Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
-                return@setOnClickListener
-            }else{
                 val hasil = ((Gaji * 1 / 173) * 1.5 * Hari) + (Gaji / 1 / 173) * 2 * (Jam - Hari)
-                val UangBaik = Gaji*5/100
+                val UangBaik = Gaji * 5 / 100
                 val GTGaji = Gaji + hasil + UangBaik + Laine
 
                 Total.setText(hasil.toInt().toString())
                 Baik.setText(UangBaik.toInt().toString())
                 Gajie.setText(GTGaji.toInt().toString())
-            }
+
+                }catch (e : Exception){
+                    Toast.makeText(this, "Data Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                }
         }
     }
 }
