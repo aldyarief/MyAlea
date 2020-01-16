25b443d2e2a193db24e9f45c6fb57c472b2bdf9f
aldryarief <aldreva@gmail.com>
2020-01-16 11:34:29 +0700

    login

--- app/src/main/java/com/example/myalea/MainActivity.kt
+++ app/src/main/java/com/example/myalea/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.myalea
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
-
+    var username: EditText? = null
+    var password:EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var button = findViewById(R.id.btn_pindah) as Button
+        var username = findViewById(R.id.username) as EditText
+        var password = findViewById(R.id.password) as EditText
+
         button.setOnClickListener() {
-            intent = Intent(this, Main2Activity::class.java)
-            startActivity(intent)
+            val usernameKey = username.text.toString()
+            val passwordKey = password.text.toString()
+            if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                //jika login berhasil
+                Toast.makeText(getApplicationContext(), "LOGIN SUKSES", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, Main2Activity::class.java)
+                startActivity(intent)
+                finish();
+            }else {
+                //jika login gagal
+                Toast.makeText(this, "Data Tidak Boleh Kosong", Toast.LENGTH_SHORT).show();
+                return@setOnClickListener
+            }
         }
     }
 }
